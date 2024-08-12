f77c5c1a93497881615225e33655bec4413bf45c
Amansharma-0106 <amansharma.am07@gmail.com>
2024-08-12 13:47:20 +0530

    Add files via upload

---
+++ Minus_plus_multiply.java
@@ -0,0 +1,9 @@
+class ABC
+{
+
+	public static void main(String args[])
+	{
+		int a=10,b=30,c=50;
+		System.out.print(a+b-c*a);
+	}
+}
---
+++ Studyjava.java
@@ -0,0 +1,14 @@
+class Studyjava {
+
+      public static void main (String [] args) 
+
+   {
+  
+               System.out.println("Aman Sharma...");
+   }
+         
+
+
+
+
+        
